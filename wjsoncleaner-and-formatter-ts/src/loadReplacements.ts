1925bbc0deaa616b24007cf2997d84fa14da796c
Alberto Sánchez Merino <albernano1@gmail.com>
2022-04-09 14:49:59 +0200

    Configuration section, load replacements from configuration

--- wjsoncleaner-and-formatter-ts/src/loadReplacements.ts
+++ wjsoncleaner-and-formatter-ts/src/loadReplacements.ts
@@ -9,23 +9,39 @@ import * as vscode from 'vscode';
 export function loadReplacements(context: vscode.ExtensionContext): string[][] {
 
     try {
-        
     
-    let fullFilePath: string = context.asAbsolutePath(path.join('resources', 'replacements.txt'));
-    let fileContent = fs.readFileSync(fullFilePath,'utf8');
+        let fullFilePath: string = context.asAbsolutePath(path.join('resources', 'replacements.txt'));
+        let fileContent = fs.readFileSync(fullFilePath,'utf8');
 
-    let replacements: string[][] = [];
+        let configuredReplacements: String|undefined ="";
+        
+        if (vscode.workspace.getConfiguration('WJC(WayletJsonCleaner).properties').has('wjc.textReplacments')){
+            configuredReplacements = vscode.workspace.getConfiguration('WJC(WayletJsonCleaner).properties').get('wjc.textReplacments');
+        }else{
+            throw new Error("Nothing to retrieve from extension configuration");
+        }
 
-    fileContent.split('\r\n').forEach(line => {
-        let selectedPard: string[] = line.split(';');
-        replacements.push([selectedPard[0], selectedPard[1]]);
-    });
+        let replacements: string[][] = [];
+        let replacementsObjetc: Object = {};
 
-    return replacements;
+        if(configuredReplacements !== ""){
+            replacementsObjetc = JSON.parse(String(configuredReplacements));
+        }
+
+        // fileContent.split('\r\n').forEach(line => {
+        //     let selectedPard: string[] = line.split(';');
+        //     replacements.push([selectedPard[0], selectedPard[1]]);
+        // });
+
+        Object.entries(replacementsObjetc).forEach(propertie =>{
+            replacements.push([propertie[0], propertie[1]]);
+        });
+
+        return replacements;
 
     } catch (error: any){
-        console.log("WJC - There's been an error reading replacement file: "+ error.message);
-        throw new Error("There's been an error reading replacement file: "+ error.message);
+        console.log("WJC - There's been an error reading replacements: "+ error.message);
+        throw new Error("There's been an error reading replacements: "+ error.message);
     }
 }
 
